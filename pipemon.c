d681975dd813327bd4654f29549709107f6a72d3
Charles Chiou <samurai@selfso.com>
2021-07-10 00:57:25 +0800

    Add calculation of round trip time
    Signed-off-by: Charles Chiou <samurai@selfso.com>

--- pipemon.c
+++ pipemon.c
@@ -90,8 +90,12 @@ int main(int argc, char **argv)
             char buf[1024];
             fd_set readfds;
             struct timeval ssh_timeout;
+            struct timeval t_send;
+            struct timeval rtt;
 
             sleep(interval);
+
+            gettimeofday(&t_send, NULL);
 
             size = write(pin[1], "hello\0", 6);
             if (size != 6) {
@@ -117,18 +121,36 @@ int main(int argc, char **argv)
             }
 
             gettimeofday(&now, NULL);
+
+            if (now.tv_usec < t_send.tv_usec) {
+                int nsec = (t_send.tv_usec - now.tv_usec) / 1000000 + 1;
+                t_send.tv_usec -= 1000000 * nsec;
+                t_send.tv_sec += nsec;
+            }
+
             conntime = now.tv_sec - since.tv_sec;
             secs = conntime % 60;
             mins = conntime / 60;
             mins %= 60;
             hours = conntime / 3600;
-            hours %= 60;
+            hours %= 24;
             days = conntime / 86400;
+
+            if (now.tv_usec - t_send.tv_usec > 1000000) {
+                int nsec = (now.tv_usec - t_send.tv_usec) / 1000000;
+                t_send.tv_usec += 1000000 * nsec;
+                t_send.tv_sec -= nsec;
+            }
+
+            rtt.tv_sec = now.tv_sec - t_send.tv_sec;
+            rtt.tv_usec = now.tv_usec - t_send.tv_usec;
+
             if (days > 0) {
-                printf("alive: %lud:%.2lu:%.2lu:%.2lu\n",
-                       days, hours, mins, secs);
+                printf("alive: %lud:%.2lu:%.2lu:%.2lu (rtt=%lu.%.6lus)\n",
+                       days, hours, mins, secs, rtt.tv_sec, rtt.tv_usec);
             } else {
-                printf("alive: %.2lu:%.2lu:%.2lu\n", hours, mins, secs);
+                printf("alive: %.2lu:%.2lu:%.2lu (rtt=%lu.%.6lus)\n",
+                       hours, mins, secs, rtt.tv_sec, rtt.tv_usec);
             }
         } while(1);
     }
